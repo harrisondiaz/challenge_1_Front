4ef66c2fecc809cc342bff744788e0fe4d696210
Harrison Diaz <hdiaz6@bancodebogota.com>
2025-02-22 12:59:38 -0500

    fix: update API URL to production endpoint in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
+    apiUrl: 'https://tasks-manager-backend-51vo.onrender.com'
+  //apiUrl: 'http://localhost:3000'
 };
